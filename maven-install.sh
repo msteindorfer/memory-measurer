e32206b9c7e36317fc147d2bcb5adae68658e428
Michael Steindorfer <Michael.Steindorfer@cwi.nl>
2016-08-18 16:05:19 +0200

    Removing clutter, nativly supporting Maven.

--- maven-install.sh
+++
@@ -1,7 +0,0 @@
-mvn install:install-file \
-  -DgroupId=com.google \
-  -DartifactId=memory-measurer \
-  -Dpackaging=jar \
-  -Dversion=1.0-SNAPSHOT \
-  -Dfile=dist/object-explorer.jar \
-  -DgeneratePom=true
